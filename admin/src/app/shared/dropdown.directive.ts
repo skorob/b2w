7b1b35f27cffb624dea240a4eacd571692a23952
Oleg Skorobogagtov <os@ipr.lv>
2018-05-15 13:17:51 +0200

    93

---
+++ admin/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,23 @@
+import {Directive, HostListener, HostBinding, ElementRef} from "@angular/core";
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+
+  @HostBinding('class.show') isOpen = false;
+
+  constructor(private elRef:ElementRef) {}
+
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+    let submenu = this.elRef.nativeElement.querySelector('.dropdown-menu')
+    if(this.isOpen) {
+      submenu.classList.add("show")
+    } else {
+      submenu.classList.remove("show")
+    }
+    console.log(submenu);
+  }
+}
