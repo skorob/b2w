22846847f0755ffa017cad014bf186e5791fbf59
Oleg Skorobogagtov <os@ipr.lv>
2018-05-16 13:18:50 +0200

    109

--- admin/src/app/recipes/recipe.service.ts
+++ admin/src/app/recipes/recipe.service.ts
@@ -1,5 +1,9 @@
 import {Recipe} from "./recipe.model";
+import {EventEmitter} from "@angular/core";
 export class RecipeService {
+
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes:Recipe[]=[
     new Recipe('A test recipe 1', 'This is simply a test','http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
     new Recipe('A test recipe 2', 'This is simply a test','http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg')
