2ef0a93ab9294970616e48825667c4f02e97690c
Oleg Skorobogagtov <os@ipr.lv>
2018-05-16 13:49:17 +0200

    110

--- admin/src/app/recipes/recipe.service.ts
+++ admin/src/app/recipes/recipe.service.ts
@@ -1,16 +1,31 @@
 import {Recipe} from "./recipe.model";
-import {EventEmitter} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
+
+@Injectable()
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes:Recipe[]=[
-    new Recipe('A test recipe 1', 'This is simply a test','http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
-    new Recipe('A test recipe 2', 'This is simply a test','http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg')
+    new Recipe('A test recipe 1', 'This is simply a test','http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg',
+      [ new Ingredient('Meat',1),
+        new Ingredient('French Fries',20)]),
+    new Recipe('A test recipe 2', 'This is simply a test','http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg',
+      [ new Ingredient('Buns',2),
+        new Ingredient('Meat',1)])
   ];
+
+  constructor(private slService : ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+
   }
 
 }
