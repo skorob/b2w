7e85a9f99033076d2a67518bbadc38d18c91a816
Oleg Skorobogagtov <os@ipr.lv>
2018-06-06 19:55:38 +0300

    156

--- admin/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ admin/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 import {log} from "util";
 
 @Component({
@@ -15,7 +15,8 @@ export class RecipeDetailComponent implements OnInit {
   id: number;
 
   constructor(private recipeService : RecipeService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     const id = this.route.params.subscribe(
@@ -26,8 +27,14 @@ export class RecipeDetailComponent implements OnInit {
     )
   }
 
+
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
   }
 
+
+  onEditRecipe() {
+    this.router.navigate(['../',this.id,'edit'], {relativeTo:this.route});
+
+  }
 }
