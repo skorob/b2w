97df68fe24ab8fdc67080bccf6a4628c86c35905
Oleg Skorobogagtov <os@ipr.lv>
2018-07-08 21:24:17 +0300

    Distr mockup

--- ui/src/app/core/logged-user/business-profile/business-profile-routing.module.ts
+++ ui/src/app/core/logged-user/business-profile/business-profile-routing.module.ts
@@ -4,10 +4,12 @@ import {RouterModule, Routes} from "@angular/router";
 
 import {EditMyBusinessPartnersComponent} from "./component/edit-my-business-partners/edit-my-business-partners.component";
 import {EditBusinessProfileComponent} from "./component/edit-business-profile/edit-business-profile.component";
+import {EditMyClientsComponent} from "./component/edit-my-clients/edit-my-clients.component";
 
 const businessProfileRoutes: Routes = [
   {path : 'edit-my-business-partners', component: EditMyBusinessPartnersComponent },
-  {path : 'edit-business-profile', component: EditBusinessProfileComponent }
+  {path : 'edit-business-profile', component: EditBusinessProfileComponent },
+  {path : 'edit-my-clients', component: EditMyClientsComponent }
 ];
 
 
--- ui/src/app/core/logged-user/business-profile/business-profile.module.ts
+++ ui/src/app/core/logged-user/business-profile/business-profile.module.ts
@@ -7,18 +7,28 @@ import {CommonModule} from "@angular/common";
 import {EditBusinessProfileComponent} from "./component/edit-business-profile/edit-business-profile.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SharedModule} from "../../../../shared/shared.module";
+import { EditMyClientsComponent } from './component/edit-my-clients/edit-my-clients.component';
+import { EditMyClientWithAddressComponent } from './component/edit-my-client-with-address/edit-my-client-with-address.component';
+import {AgmCoreModule} from "@agm/core";
 
 
 @NgModule({
   declarations: [
       EditMyBusinessPartnersComponent,
-      EditBusinessProfileComponent
+      EditBusinessProfileComponent,
+      EditMyClientsComponent,
+      EditMyClientWithAddressComponent
   ],
   imports: [
     FormsModule,
     SharedModule,
     ReactiveFormsModule,
-    BusinessProfileRoutingModule
+    BusinessProfileRoutingModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDNBikVW8Hx7-ntW3HI2iHYoKBjuXodkZs',
+      libraries: ["places"]
+    })
   ],
   exports: [
     EditBusinessProfileComponent
